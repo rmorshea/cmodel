67b419a24d56891e8dcf6e00908256b34d2c558e
Ryan Morshead <ryan.morshead@gmail.com>
2026-04-01 00:38:41 -0600

    lint

--- python/cmodel/__init__.py
+++ python/cmodel/__init__.py
@@ -5,7 +5,7 @@ from cmodel import types as types
 from cmodel.base import CFmt as CFmt
 from cmodel.base import CModel as CModel
 
-try:  # noqa: RUF067
+try:
     __version__ = version(__name__)
 except PackageNotFoundError:  # nocov
     __version__ = "0.0.0"
